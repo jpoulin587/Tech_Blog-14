aaa4b86afb0ad15c41a10f5721a358de4337c906
jpoulin587 <jpoulin587a@gmail.com>
2021-08-27 10:55:18 -0500

    article post route fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const articleRoutes = require('./articleRoutes');
 
 router.use('/users', userRoutes);
+router.use('/dashboard', articleRoutes);
 
 module.exports = router;
