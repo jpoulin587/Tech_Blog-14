78e403c1e116833a688a9f5836d86b26d6f3d774
jpoulin587 <jpoulin587a@gmail.com>
2021-08-27 14:03:19 -0500

    delete article working

--- controllers/api/articleRoutes.js
+++ controllers/api/articleRoutes.js
@@ -15,7 +15,23 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-
+router.delete('/:id', withAuth, async (req,res) =>{
+  try {
+    const articleData = await Article.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!articleData) {
+      res.status(404).json({message: 'No project found with this id.'});
+      return;
+    }
+    res.status(200).json(articleData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,9 +21,27 @@ const newFormHandler = async (event) => {
   }
 };
 
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+console.log(id);
+    const response = await fetch(`/api/dashboard/${id}`, {
+      method: 'DELETE'
+    });
 
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('failed to delete project');
+    }
+  }
+};
 
 
 document
   .querySelector('.new-article-form')
   .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.article-list')
+  .addEventListener('click', delButtonHandler);
