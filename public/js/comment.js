7c015429953f70a3690ebf23e60bb73d278ffa0f
jpoulin587 <jpoulin587a@gmail.com>
2021-08-28 10:53:47 -0500

    working on comment bugs

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,25 +1,25 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-const comment = document.querySelector('#comment-body').value.trim();
+  event.preventDefault();
 
-if (comment) {
+  const comment = document.querySelector('#comment-body').value.trim();
+
+  if (comment) {
     const response = await fetch('/api/comment', {
-        method: 'POST',
-        body: JSON.stringify({comment}),
-        headers: {
-            'Content-Type': 'application/json',
-        };
+      method: 'POST',
+      body: JSON.stringify({comment}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
     if (response.ok) {
-        document.location.replace('/article');
+      document.location.replace('/article');
     } else {
-        alert('failed to create comment');
+      alert('failed to create comment');
     }
-}
+  }
 };
 
 
 document
-.querySelector('.new-article-form')
-.addEventListener('submit', newFormHandler);
+  .querySelector('.new-article-form')
+  .addEventListener('submit', newFormHandler);
