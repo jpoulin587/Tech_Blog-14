12c3eba7506179477d851725ac7a27afb6c741ac
jpoulin587 <jpoulin587a@gmail.com>
2021-08-28 19:40:46 -0500

    new user sign up built

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,20 @@ const { post } = require('../homeRoutes');
 
 
 // TODO create new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
 
